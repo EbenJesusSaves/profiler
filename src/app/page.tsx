b59a19a4e97996f4f14df9e9e8daa270742a2129
Eben <96250954+EbenJesusSaves@users.noreply.github.com>
2024-05-17 22:26:45 +0000

    testing how the new design will look like

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,40 +12,44 @@ import s from "./home/Home.module.css";
 import aboutImg from "../../public/avatars/Dynamic.png";
 import { GoogleGeminiEffectComponent } from "@/components/home/ColorsScrollEffect";
 import { GetInTouch } from "@/components/home/Forms";
+import { Fragment } from "react";
 export default function Home() {
   return (
-    <>
-      <div className={s.root}>
-        <TracingBeam>
+    <div className="flex  items-center flex-col ">
+      {/* <div className={s.root}> */}
+      {/* <TracingBeam> */}
+      <div className="bg-[#0e0e10] w-full flex  flex-col items-center">
+        <SparklesPreview />
+        <div className="flex justify-center flex-wrap-reverse lg:flex-nowrap">
           <div>
-            <SparklesPreview />
-            <div className="flex justify-center flex-wrap-reverse lg:flex-nowrap">
-              <div>
-                <TextRevealCardPreview />
-                <div className="w-full bg-black mt-[8rem] flex flex-row gap-5 items-center justify-center">
-                  <MovingBorderDemo text={"download CV"} />
-                  <MovingBorderDemo text={"contact me"} />
-                </div>
-              </div>
-            </div>
-            <Aboutme />
-            <div className="container max-width-2xl">
-              {/* <MacbookScrollDemo /> */}
-              <TypewriterEffectSmoothDemo />
-              <InfiniteMovingCardsDemo />
-            </div>
-            <div className="container lg:w-[100vw] bg-slate-500"></div>
-            <HeroParallaxDemo />
-          </div>
-          <GoogleGeminiEffectComponent />
-          <div className="flex justify-around items-center">
-            <div className="bg-green-400">First div</div>
-            <div className="bg-green-400">
-              <GetInTouch />
+            <TextRevealCardPreview />
+            <div className="w-full bg-black mt-[8rem] flex flex-row gap-5 items-center justify-center">
+              <MovingBorderDemo text={"download CV"} />
+              <MovingBorderDemo text={"contact me"} />
             </div>
           </div>
-        </TracingBeam>
+        </div>
+        <Aboutme />
+        <div className="flex flex-col  container max-width-2xl justify-center items-center">
+          {/* <MacbookScrollDemo /> */}
+          <TypewriterEffectSmoothDemo />
+          <InfiniteMovingCardsDemo />
+        </div>
       </div>
-    </>
+
+      <div className="container flex justify-center bg-white align-middle">
+        <HeroParallaxDemo />
+      </div>
+
+      <GoogleGeminiEffectComponent />
+      <div className="flex justify-around items-center bg-black w-full">
+        <div className="bg-green-400">First div</div>
+        <div className="bg-green-400">
+          <GetInTouch />
+        </div>
+      </div>
+      {/* </TracingBeam> */}
+      {/* </div> */}
+    </div>
   );
 }
--- src/components/home/projects.tsx
+++ src/components/home/projects.tsx
@@ -1,100 +1,88 @@
 "use client";
 import React from "react";
 import { HeroParallax } from "../ui/movingPages";
-import u1 from '../../../public/images/Untitled-1.webp'
-import u2 from '../../../public/images/Untitled-2.webp'
-import u3 from '../../../public/images/Untitled-3.webp'
-import u4 from '../../../public/images/Untitled-4.webp'
-import u5 from '../../../public/images/Untitled-5.webp'
-import u6 from '../../../public/images/Untitled-6.webp'
-import u7 from '../../../public/images/Untitled-7.webp'
+import u1 from "../../../public/images/Untitled-1.webp";
+import u2 from "../../../public/images/Untitled-2.webp";
+import u3 from "../../../public/images/Untitled-3.webp";
+import u4 from "../../../public/images/Untitled-4.webp";
+import u5 from "../../../public/images/Untitled-5.webp";
+import u6 from "../../../public/images/Untitled-6.webp";
+import u7 from "../../../public/images/Untitled-7.webp";
 
 export function HeroParallaxDemo() {
-    return <HeroParallax products={products} />;
+  return <HeroParallax products={products} />;
 }
 export const products = [
-    {
-        title: "Deal Analyzer",
-        link: "https://gomoonbeam.com",
-        thumbnail:
-            u1,
-    },
-    {
-        title: "Cursor",
-        link: "https://cursor.so",
-        thumbnail:
-            u2,
-    },
-    {
-        title: "Rogue",
-        link: "https://userogue.com",
-        thumbnail:
-            u3,
-    },
-    {
-        title: "Moonbeam",
-        link: "https://gomoonbeam.com",
-        thumbnail:
-            u1,
-    },
-    {
-        title: "Cursor",
-        link: "https://cursor.so",
-        thumbnail:
-            u2,
-    },
-    {
-        title: "Rogue",
-        link: "https://userogue.com",
-        thumbnail:
-            u3,
-    },
+  {
+    title: "Deal Analyzer",
+    link: "https://gomoonbeam.com",
+    thumbnail: u1,
+  },
+  {
+    title: "Cursor",
+    link: "https://cursor.so",
+    thumbnail: u2,
+  },
+  {
+    title: "Rogue",
+    link: "https://userogue.com",
+    thumbnail: u3,
+  },
+  {
+    title: "Moonbeam",
+    link: "https://gomoonbeam.com",
+    thumbnail: u1,
+  },
+  {
+    title: "Cursor",
+    link: "https://cursor.so",
+    thumbnail: u2,
+  },
+  {
+    title: "Rogue",
+    link: "https://userogue.com",
+    thumbnail: u3,
+  },
 
-    {
-        title: "Editorially",
-        link: "https://editorially.org",
-        thumbnail:
-            u4,
-    },
-    {
-        title: "Editrix AI",
-        link: "https://editrix.ai",
-        thumbnail:
-            u5
-    },
-    {
-        title: "Pixel Perfect",
-        link: "https://app.pixelperfect.quest",
-        thumbnail:
-            u6
-    },
+  {
+    title: "Editorially",
+    link: "https://editorially.org",
+    thumbnail: u4,
+  },
+  {
+    title: "Editrix AI",
+    link: "https://editrix.ai",
+    thumbnail: u5,
+  },
+  {
+    title: "Pixel Perfect",
+    link: "https://app.pixelperfect.quest",
+    thumbnail: u6,
+  },
 
-    {
-        title: "Algochurn",
-        link: "https://algochurn.com",
-        thumbnail:
-            u7
-    },
-    {
-        title: "Editrix AI",
-        link: "https://editrix.ai",
-        thumbnail:
-            u5
-    }, {
-        title: "Pixel Perfect",
-        link: "https://app.pixelperfect.quest",
-        thumbnail:
-            u6
-    },
-    {
-        title: "Cursor",
-        link: "https://cursor.so",
-        thumbnail:
-            u2,
-    }, {
-        title: "Rogue",
-        link: "https://userogue.com",
-        thumbnail:
-            u3,
-    },
+  {
+    title: "Algochurn",
+    link: "https://algochurn.com",
+    thumbnail: u7,
+  },
+  {
+    title: "Editrix AI",
+    link: "https://editrix.ai",
+    thumbnail: u5,
+  },
+  {
+    title: "Pixel Perfect",
+    link: "https://app.pixelperfect.quest",
+    thumbnail: u6,
+  },
+  {
+    title: "Cursor",
+    link: "https://cursor.so",
+    thumbnail: u2,
+  },
+  {
+    title: "Rogue",
+    link: "https://userogue.com",
+    thumbnail: u3,
+  },
 ];
