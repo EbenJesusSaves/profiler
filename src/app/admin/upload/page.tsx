3b08013763d1658aabdf296f5ba36d7bb4960790
Eben <96250954+EbenJesusSaves@users.noreply.github.com>
2024-06-07 23:51:27 +0000

    added CDN upload

---
+++ src/app/admin/upload/page.tsx
@@ -0,0 +1,8 @@
+import { ImageString } from "@/components/admin/ImageString";
+import React from "react";
+
+const page = () => {
+  return <ImageString />;
+};
+
+export default page;
--- src/components/admin/ImageString.tsx
+++ src/components/admin/ImageString.tsx
@@ -1,15 +1,29 @@
+"use client";
+
 import { useState } from "react";
-import CloudinaryUploadWidget from "./CloudinaryUploadWidget";
+import CloudinaryUploadWidget from "./cWidject";
 import { Cloudinary } from "@cloudinary/url-gen";
 import { AdvancedImage, responsive, placeholder } from "@cloudinary/react";
 
 export const ImageString = () => {
+  const [publicId, setPublicId] = useState("");
+  // Replace with your own cloud name
+
+  // Replace with your own upload preset
+  const [uploadPreset] = useState("halumx55");
+  const cloudName = "djzn1iixv";
   const cld = new Cloudinary({
     cloud: {
       cloudName: "djzn1iixv",
     },
   });
 
+  const [uwConfig] = useState({
+    cloudName,
+    uploadPreset,
+  });
+  const myImage = cld.image(publicId);
+  console.log(myImage);
   return (
     <div className="App">
       <h3>Cloudinary Upload Widget Example</h3>
